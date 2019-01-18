dfcba93c7717796e0d7a6f8e34917ca5441289cc
BrunoBoehm <bruno.boehm@gmail.com>
2019-01-18 21:01:23 +0100

    Update config siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Digital School`,
+    description: `Nous vous apportons les connaisances digitales nécessaires à votre succès sur le web.`,
+    author: `@lyketil`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
